6c81dcf9e44e26eb3addac9cbdc18adc5d9ffd1a
annarehnman <63710449+annarehnman@users.noreply.github.com>
2022-10-25 12:59:54 +0200

    added token

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,17 +1,17 @@
 import { useRef, useEffect, useState } from 'react'
 import 'mapbox-gl/dist/mapbox-gl.css'
-import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+import mapboxgl from '!mapbox-gl' // eslint-disable-line import/no-webpack-loader-syntax
 
 const Map = () => {
-  mapboxgl.accessToken = 'pk.eyJ1IjoicmVobm1hbjEwMSIsImEiOiJjbDlvMDFqdWYwYnNpM25vNW85ZTU4ZXlqIn0.W_OxrGYnJK2X4PM6O63vfA'
-  const mapContainer = useRef(null);
-  const map = useRef(null);
-  const [lng] = useState(16.936264401463887);
-  const [lat] = useState(59.439098072932836);
-  const [zoom] = useState(12);
+  mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN
+  const mapContainer = useRef(null)
+  const map = useRef(null)
+  const [lng] = useState(16.936264401463887)
+  const [lat] = useState(59.439098072932836)
+  const [zoom] = useState(12)
 
   useEffect(() => {
-    if (map.current) return; // initialize map only once
+    if (map.current) return
     map.current = new mapboxgl.Map({
       container: mapContainer.current,
       style: 'mapbox://styles/mapbox/streets-v11',
